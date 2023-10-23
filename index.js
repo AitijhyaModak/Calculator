22c9fa8fb3bab7da0a3f452c083b1f3dacab8397
Aitijhya Modak <aitijhyasps1@gmail.com>
2023-10-23 18:07:24 +0530

    Remove PERCENT button

--- index.js
+++ index.js
@@ -1,41 +1,35 @@
 let number1 = "";
 let number2 = "";
 let operator = "";
-
-//handling number button clicks
-let numbers = document.querySelectorAll(".number");
-for (let number of numbers)
-    number.addEventListener("click", onNumberClick);
+let operator_pressed = false;
 
 
-function onNumberClick(event) 
+//handling number click
+
+let number_buttons = document.querySelectorAll(".number");
+for (let number_button of number_buttons)
+    number_button.addEventListener("click",onNumberClick);
+
+function onNumberClick(event)
 {
-    let number_clicked = event.srcElement.innerText;
-    if (number1 == 0)
-        number1 = number_clicked;
-    else number2 = number_clicked;
-    showSubtext();
-}
+    if (operator_pressed === false)
+        number1 = number1 + event.srcElement.innerText;
+    else 
+        number2 = number2 + event.srcElement.innerText;
 
-//handling symbol clicks
-let symbols = document.querySelectorAll(".symbol");
-for (let symbol of symbols)
-    symbol.addEventListener("click",onSymbolClick);
-
-function onSymbolClick(event)
-{
-    let operator_clicked = event.srcElement.innerText;
-    operator = operator_clicked;
-    console.log(operator);
     showSubtext();
 }
 
 
-//showing subtext at calculator
-let subtext = "";
+
+
+
+
+
+
+//showing sub text
 function showSubtext()
-{   
-    subtext = number1 + operator + number2;
+{
+    let subtext = number1 + operator + number2;
     document.querySelector("#subtext").innerText = subtext;
 }
-
